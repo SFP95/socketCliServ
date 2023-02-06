79ec2e7e8a38264b932a6b979c05bccd475b12b5
Nia <103027252+SFP95@users.noreply.github.com>
2023-02-06 13:12:04 +0100

    06.02.23

--- mensajesCifrados/MensajeEjercicio.java
+++ mensajesCifrados/MensajeEjercicio.java
@@ -6,16 +6,49 @@ import java.security.Provider;
 
 public class MensajeEjercicio {
     public static void main(String[] args) {
+
         MessageDigest md;
         try {
             md = MessageDigest.getInstance("SHA");
-            String mensaje = "/fichero.txt";
-            byte dataBytes[] = mensaje.getBytes();
-            md.update(dataBytes);
-            byte resumen[] = mensaje.digest();
+            String texto = "tengo que meter aqui el fichero."; //buscarlo en los apuntes de la primea evaluación
 
+            byte dataBytes[] = texto.getBytes();//TEXTO A BYTES
+            md.update(dataBytes);//SE INTRQDUCE TEXTO EN BYTES A RESUMIR
+            byte resumen[] = md.digest();//SE CALCULA EL RESUMEN
+
+//PARA CREAR UN RESUMEN CIFRADO CON CLAVE
+//String clave="clave de cifrado";
+//byte resumen[] = md.digest(clave.getBytes()); // SE CALCULA EL RESUMEN CIFRADO
+            System.out.println("Mensaje original: " + texto);
+            System.out.println("Número de bytes: "
+                    + md.getDigestLength());
+            System.out.println("Algoritmo: "
+                    + md.getAlgorithm());
+            System.out.println("Mensaje resumen: " + new String(resumen));
+            System.out.println("Mensaje en Hexadecimal: "
+                    + Hexadecimal(resumen));
+            Provider proveedor = md.getProvider();
+            System.out.println("Proveedor: "
+                    + proveedor.toString());
         } catch (/*NoSuchAlgorithmException |*/ NoSuchAlgorithmException e) {
-            System.out.println("ERROR en el MessageDigest" + e);
+            System.out.println("ERROR en el MessageDigest"+e);
         }
-    }
+
+    }//Fin de main
+
+    // CONVIERTE UN ARRAY DE BYTES A HEXADECIMAL
+    static String Hexadecimal(byte[] resumen) {
+
+        String hex = "";
+        for (int i = 0; i < resumen.length; i++) {
+
+            String h = Integer.toHexString(resumen[i] & 0xFF);
+            if (h.length() == 1) {
+                hex += "0";
+            }
+            hex += h;
+        }//Fin de for
+
+        return hex.toUpperCase();
+    }// Fin de Hexadecimal
 }
